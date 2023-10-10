5aff10bcec14e25d758351c30e824ec130d8d75c
Deumir404 <122665670+Deumir404@users.noreply.github.com>
2023-10-10 20:04:07 +0300

    Add files via upload

---
+++ Test_1_part_1.pas
@@ -0,0 +1,15 @@
+﻿program control_1;
+var x:integer;
+var answer:real;
+const e = 2.718281;
+begin
+  Writeln('Введите число');
+  Readln(x);
+  if x < -7 then 
+    answer := power(x, 1/3)-x
+  else  if 
+  x <= 0 
+  then answer := power(x, 2) / (0.1 * x) - x / power(e,x)
+  else answer := power(x,3) / power(e,x)
+  Writeln(answer)
+end.
---
+++ Test_1_part_2.pas
@@ -0,0 +1,20 @@
+﻿program control_2;
+var answer, x:real;
+const e = 2.718;
+begin
+  x:= -9.0;
+  while x <= 2 do
+    begin
+      if x < -7 then 
+    answer := power(x, 1/3)-x
+  else  if 
+  0 <= x
+  then answer := power(x, 2) / (0.1 * x) - x / power(e,x)
+  else answer := power(x,3) / power(e,x);
+  Writeln('Для значения ' , round(x, 2), ' значение функции равно ' , answer);
+  x:= x + 0.1;
+    end; 
+ 
+  
+   
+end.
